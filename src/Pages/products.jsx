c935a760cc60bb3f78c59391c9e8c8c370cb6a2d
rifqee23 <rifqifebrianto746@gmail.com>
2024-07-25 16:06:09 +0700

    rendering list

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,18 +1,40 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
-const ProductPages = () => {
-  return (
-    <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/beautiful-blonde-girl-white-oversized-tshirt-blue-jeans-posing-gray-background.jpg" />
-        <CardProduct.Body title="Baju baru">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
+const products = [
+  {
+    id: 1,
+    image:
+      "/images/beautiful-blonde-girl-white-oversized-tshirt-blue-jeans-posing-gray-background.jpg",
+    name: "Baju Wanita",
+    price: "Rp 100.000",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
           inventore natus, aliquid dicta obcaecati repudiandae consectetur
           aliquam cupiditate repellendus odio nisi perferendis quas? Reiciendis
-          quasi fugit explicabo, beatae ullam corrupti!
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 100.000">Add to chart</CardProduct.Footer>
-      </CardProduct>
+          quasi fugit explicabo, beatae ullam corrupti!`,
+  },
+  {
+    id: 2,
+    image: "/images/teenage-boy-white-tee-basic-youth-apparel-shoot.jpg",
+    name: "Baju Pria",
+    price: "Rp 80.000",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. `,
+  },
+];
+
+const ProductPages = () => {
+  return (
+    <div className="flex justify-center py-5 gap-8">
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body title={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price}>
+            Add to chart
+          </CardProduct.Footer>
+        </CardProduct>
+      ))}
     </div>
   );
 };
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="w-full max-w-xs bg-gray-800 border-gray-200 rounded-lg shadow py-2 ">
+    <div className="w-full max-w-xs bg-gray-800 border-gray-200 rounded-lg shadow py-2 flex flex-col justify-between">
       {children}
     </div>
   );
@@ -28,7 +28,7 @@ const Header = (props) => {
 const Body = (props) => {
   const { children, title } = props;
   return (
-    <div className="px-5">
+    <div className="px-5 h-full">
       <a href="#">
         <h5 className="text-xl font-semibold tracking-tight text-white">
           {title}
